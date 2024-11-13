a3fb62a549c85bd07de4a5eaecaa9c8e0cfc6311
darvfus <romerondan123@gmail.com>
2024-11-13 06:34:44 -0600

    comple the task but i have mmistake in images

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,13 +1,16 @@
-const utilies = require("../utilities/")
+const utilities = require("../utilities/")
 const baseController ={}
 
 /***********************************
  * Build the Home view with MVC 
  * unit3, Activities
  ***********************************/
-baseController.buildHome = async function(req,res){
-    const nav = await utilies.getNav()
+baseController.buildHome = async function(req, res){
+    const nav = await utilities.getNav()
     res.render("index", {title: "Home", nav})
-}
+  }
 
+  baseController.footerErr = async function (req, res) {
+    res.render("/par/footer", "Yebo")
+  }
 module.exports = baseController 
--- server.js
+++ server.js
@@ -8,13 +8,17 @@
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
-const baseControler = require("./controllers/baseController") 
+const baseController = require("./controllers/baseController") 
 const pool = require("./database")
 const static = require("./routes/static")
-const utilies = require("./utilities")
+const utilities = require("./utilities")
 const inventoryRoute = require("./routes/inventoryRoute")
 
 const app = express()
+
+/******************/
+
+
 /* ***********************
  * View Engine and Templates
  *************************/
@@ -28,9 +32,28 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 
 //index route
-app.get("/", baseControler.buildHome) 
+app.get("/", utilities.handleErrors(baseController.buildHome))
 // Inventory routes
 app.use("/inv", inventoryRoute)
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page. Or it got devoured by Gremlins👀'})
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
 
 /* ***********************
  * Local Server Information
